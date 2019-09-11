91bcfa372f5f51167261d979b4ecdef5578a71b8
llllou <llllou114@gmail.com>
2019-09-11 18:57:10 +0800

    分支节点

--- src/flow/component/switch_node.tsx
+++ src/flow/component/switch_node.tsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
-
-export default class SwitchNode extends Component<any,any> {
-  constructor(props:any){
+import React, { Component, PureComponent } from 'react'
+import {List,Icon} from "antd"
+export default class SwitchNode extends PureComponent<{onRemove:Function},any> {
+  constructor(props:{onRemove:Function}){
     super(props)
-    this.state = {
-      
-    }
   }
   render() {
     return (
-      <div>
-        
+      <div className="flow-block">
+        <div className="switch-node">
+          <Icon type="close-circle" className="node-close" onClick={this.props.onRemove()}></Icon>
+          <List>
+            <List.Item>所有数据可进入该分支</List.Item>  
+            <List.Item>筛选数据</List.Item>  
+          </List>  
+        </div>
+        <div className="flow-line"></div>
       </div>
     )
   }
